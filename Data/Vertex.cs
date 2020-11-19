6ec1de8b6bc98cf274a6576fbacb36c5ab8c6bfb
Samuel Egger <se@adaris.ch>
2020-11-20 00:43:43 +0100

    Minor refactorings.

---
+++ Data/Vertex.cs
@@ -0,0 +1,12 @@
+﻿using Sr3D.SrMath;
+using System.Drawing;
+
+namespace SoftwareRenderer3D.Data
+{
+    public struct Vertex
+    {
+        public Vector3 Position;
+
+        public PointF Texture;
+    }
+}
--- Graphics/Scene.cs
+++ Graphics/Scene.cs
@@ -5,6 +5,12 @@ namespace Sr3D.Graphics
 {
     public class Scene
     {
+        public const float RotInc = 0.1047f;
+
+        public float RotX = 0.0f;
+
+        public float RotY = 0.0f;
+
         public IndexedFaceSet Model { get; set; }
 
         public Matrix4x4 Transform { get; set; } = Matrix4x4.Identity;

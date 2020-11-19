9a7f125fb6e8fe66ef4ebed412005f163be8e4fd
Samuel Egger <se@adaris.ch>
2020-11-20 00:39:20 +0100

    Refactor and extend math functions.

---
+++ Math/SrMathUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SoftwareRenderer3D.SrMath
+{
+    public class SrMathUtils
+    {
+        public const float TwoPI = 2 * (float)Math.PI;
+        public const float PI = (float)Math.PI;
+
+        public static float SanitizeAngle(float angle)
+        {
+            angle %= TwoPI;
+            if(angle > PI)
+            {
+                angle -= TwoPI;
+            }
+
+            return angle;
+        }
+
+        public static float DegreeToRadian(float degree)
+        {
+            return (float)(degree * System.Math.PI / 180);
+        }
+
+        public static float Perp(System.Numerics.Vector2 a, System.Numerics.Vector2 b)
+        {
+            return System.Numerics.Vector2.Dot(new System.Numerics.Vector2(-a.Y, a.X), b);
+        }
+
+        /// <summary>
+        /// The perp product is equal to |a|*|b|*sin(alpha). Sometimes referred to as the 2D equivalent of the cross product.
+        /// See <seealso cref="https://www.geogebra.org/calculator/whbtnbex"/> for simulation.
+        /// </summary>
+        /// <param name="a">The vector a.</param>
+        /// <param name="b">The vector b.</param>
+        /// <returns>The perp product of vector a and b.</returns>
+        /// <remarks>https://mathworld.wolfram.com/PerpDotProduct.html</remarks>
+        public static float Perp(SoftwareRenderer3D.SrMath.Vector2 a, SoftwareRenderer3D.SrMath.Vector2 b)
+        {
+            return SoftwareRenderer3D.SrMath.Vector2.Dot(new SoftwareRenderer3D.SrMath.Vector2(-a.Y, a.X), b);
+        }
+    }
+}
---
+++ Math/Vector2.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.X86;
+
+namespace SoftwareRenderer3D.SrMath
+{
+    public struct Vector2
+    {
+        public float X;
+        public float Y;
+
+        public Vector2(float value) : this(value, value) { }
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static unsafe float Dot(Vector2 a, Vector2 b)
+        {
+            float[] vecA = new float[] { a.X, a.Y };
+
+            //if (Avx2.IsSupported)
+            //{
+            //    fixed (float* pA = vecA)
+            //    {
+            //        Sse41.DotProduct
+            //        Avx2.DotProduct()
+            //    }
+            //}
+            //else
+            //{
+            //}
+
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2 operator -(Vector2 a, Vector2 b) => new Vector2(a.X - b.X, a.Y - b.Y);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2 operator *(Vector2 a, float b) => new Vector2(a.X * b, a.Y * b);
+    }
+}

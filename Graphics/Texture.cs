6eceb0be6d8115fd6a2128a8dbf984c21437adea
Samuel Egger <se@adaris.ch>
2020-11-20 17:29:22 +0100

    Add new Texture class.

---
+++ Graphics/Texture.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+
+namespace CSharpRenderer.Graphics
+{
+    public class Texture : IDisposable
+    {
+        private readonly Image image;
+        private bool isDisposed;
+
+        public Texture(Image image)
+        {
+            this.image = image;
+        }
+
+        public static Texture FromFile(string fileName)
+        {
+            var image = Image.FromFile(fileName);
+            return new Texture(image);
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            // https://docs.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1063?view=vs-2019
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                image.Dispose();
+            }
+
+            isDisposed = true;
+        }
+        #endregion IDisposable
+    }
+}

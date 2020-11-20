eec06c1ac9507d6613dc4484c95a8adc0930f068
Samuel Egger <se@adaris.ch>
2020-11-21 00:44:11 +0100

    Add benchmark project.

---
+++ CSharpRenderer.Benchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CSharpRenderer.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
---
+++ CSharpRenderer.Benchmark/RasterizerBenchmark.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using CSharpRenderer.Core;
+using CSharpRenderer.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace CSharpRenderer.Benchmark
+{
+    public class RasterizerBenchmark : IDisposable
+    {
+        private bool isDisposed;
+        private const int Height = 768;
+        private const int Width = 1024;
+        
+        private readonly Surface surface;
+        private readonly EdgeRasterizer edgeRasterizer;
+        private IntPtr buffer;
+        private readonly Random rnd = new Random();
+
+        public RasterizerBenchmark()
+        {
+            buffer = Marshal.AllocHGlobal(sizeof(int));
+            surface = new Surface(buffer, 0, 0, Width, Height);
+            edgeRasterizer = new EdgeRasterizer(surface);
+        }
+
+        public IEnumerable<object> Triangle()
+        {
+            yield return new Int32Point[] { new Int32Point(100, 70), new Int32Point(50, 50), new Int32Point(60, 100) };
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Triangle))]
+        public void EdgeRasterizer(Int32Point[] triangle)
+        {
+            edgeRasterizer.FillTriangle(Color.White, triangle, triangle /* not used anyway */);
+        }
+
+        public void Dispose()
+        {
+            DisposeInternal();
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void DisposeInternal()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            if (buffer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(buffer);
+                buffer = IntPtr.Zero;
+            }
+
+            isDisposed = true;
+        }
+
+        ~RasterizerBenchmark()
+        {
+            DisposeInternal();
+        }
+    }
+}
